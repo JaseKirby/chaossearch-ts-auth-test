660344b45b2e271b401b0a3b731efcabd9e370e9
Jase Kirby <jase.kirby@blackboard.com>
2020-12-10 16:50:46 -0500

    GET working with V1 endpoint, POSTS broken with 403

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as awsv4 from 'aws4';
+import * as aws4 from 'aws4';
 import axios from 'axios';
 
 const host = process.env.CS_HOST || '';
@@ -6,12 +6,47 @@ const accessKeyId = process.env.CS_API_KEY_ID || '';
 const secretAccessKey = process.env.CS_API_SECRET_KEY || '';
 const bucketName = process.env.CS_BUCKET || '';
 
-export function chFullReq(host: string, accessKeyId: string, secretAccessKey: string, bucketName: string) {
+csV1(host, accessKeyId, secretAccessKey);
+console.log('=====================csV1-end=============================\n\n\n');
 
-    const reqOpts = awsv4.sign({
+csBucketMetadata(host, accessKeyId, secretAccessKey, bucketName);
+console.log('=====================csBucketMetadata-end=================\n\n\n');
+
+csObjGroup(host, accessKeyId, secretAccessKey, bucketName);
+console.log('=====================csObjGroup-end=======================\n\n\n');
+
+export function csV1(host: string, accessKeyId: string, secretAccessKey: string) {
+    const reqOpts = aws4.sign({
+        host: 'blackboard-us-east-1.chaossearch.io',
+        service: 's3',
+        region: 'us-east-1',
+        path: '/V1/'
+    }, {
+        secretAccessKey: secretAccessKey,
+        accessKeyId: accessKeyId
+    });
+
+    console.log(reqOpts);
+
+    const resp = axios.request({
+        url: `https://${host}/V1/`,
+        method: 'GET',
+        headers: reqOpts.headers
+    });
+
+    resp.then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.error(err);
+    });
+}
+
+export function csBucketMetadata(host: string, accessKeyId: string, secretAccessKey: string, bucketName: string) {
+    const reqOpts = aws4.sign({
         host: host,
         service: 's3',
         region: 'us-east-1',
+        path: '/Bucket/metadata/'
     }, {
         accessKeyId: accessKeyId,
         secretAccessKey: secretAccessKey
@@ -20,7 +55,7 @@ export function chFullReq(host: string, accessKeyId: string, secretAccessKey: st
     console.log(reqOpts);
 
     const resp = axios.request({
-        url: `https://${host}/Bucket/metadata`,
+        url: `https://${host}/Bucket/metadata/`,
         method: 'POST',
         headers: reqOpts.headers,
         data: {
@@ -32,7 +67,50 @@ export function chFullReq(host: string, accessKeyId: string, secretAccessKey: st
         console.log(res);
     }).catch((err) => {
         console.error(err);
-    })
+    });
 }
 
-chFullReq(host, accessKeyId, secretAccessKey, bucketName);
+csBucketMetadata(host, accessKeyId, secretAccessKey, bucketName);
+
+export function csObjGroup(host: string, accessKeyId: string, secretAccessKey: string, bucketName: string) {
+    const csObjGroup = {
+        bucket: `${bucketName}-int-test`,
+        source: bucketName,
+        indexRetention: 14,
+        format: {
+            _type: 'JSON',
+            stripPrefix: true,
+            horizontal: true
+        },
+        filter: { "AND": [{ "field": "key", "prefix": "ecs/" }, { "field": "key", "regex": ".*" }] },
+        options: {
+            ignoreIrregular: true,
+            compression: 'GZIP'
+        }
+    };
+
+    const reqOpts = aws4.sign({
+        host: host,
+        service: 's3',
+        region: 'us-east-1',
+        path: '/Bucket/createObjectGroup/'
+    }, {
+        secretAccessKey: secretAccessKey,
+        accessKeyId: accessKeyId
+    });
+
+    console.log(reqOpts);
+
+    const resp = axios.request({
+        url: `https://${host}/Bucket/createObjectGroup/`,
+        method: 'POST',
+        headers: reqOpts.headers,
+        data: csObjGroup
+    });
+
+    resp.then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.error(err);
+    });
+}
